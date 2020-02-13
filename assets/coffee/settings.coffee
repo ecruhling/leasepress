6ea819f33a0edfd7e5245485b601508a56fbaede
erik <erik@resourceatlanta.com>
2020-02-13 11:20:27 -0500

    hook button to ajax action

--- assets/coffee/settings.coffee
+++ assets/coffee/settings.coffee
@@ -2,7 +2,19 @@
 	'use strict'
 	$ ->
 		$('#tabs').tabs()
-	# Place your administration-specific JavaScript here
-		console.log 'works'
+		# Place your administration-specific JavaScript here
+		$('#lp_api_floorplans_lookup_submit').on 'click', (event) ->
+			(event).preventDefault()
+			console.log 'works'
+			$.ajax
+				url: ajaxurl
+				data: {
+					action: 'get_data',
+				},
+				error: (jqXHR, textStatus, errorThrown) ->
+					console.log(jqXHR, textStatus, errorThrown)
+				success: (data, textStatus, jqXHR) ->
+					console.log(data, textStatus, jqXHR)
+			return
 	return
 ) jQuery
--- assets/js/settings.js
+++ assets/js/settings.js
@@ -3,7 +3,22 @@
   $(function() {
     $('#tabs').tabs();
     // Place your administration-specific JavaScript here
-    return console.log('works');
+    return $('#lp_api_floorplans_lookup_submit').on('click', function(event) {
+      event.preventDefault();
+      console.log('works');
+      $.ajax({
+        url: ajaxurl,
+        data: {
+          action: 'get_data'
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+          return console.log(jqXHR, textStatus, errorThrown);
+        },
+        success: function(data, textStatus, jqXHR) {
+          return console.log(data, textStatus, jqXHR);
+        }
+      });
+    });
   });
 })(jQuery);
 
